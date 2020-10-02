14e27cdc9bc596219cc6aaf63c085179d4284bdf
vagrant <furukawa12195656@gmail.com>
2020-10-02 18:34:11 +0000

    finish4-3.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
-
   # ページごとの完全なタイトルを返します。
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
+  def full_title(page_title = '')  #メソッド名に引数を設定　引数の値にデフォルト値を設定（空）
+    base_title = "Ruby on Rails Tutorial Sample App"  #変数の設定
+    if page_title.empty?  #論理値テスト
       base_title
     else
       page_title + " | " + base_title
